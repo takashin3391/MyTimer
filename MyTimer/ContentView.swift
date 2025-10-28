902867137037dc66f4c60ae4214513b134f26cfb
Takayama <tkym1982@gmail.com>
2025-10-28 20:50:24 +0900

    Initial Commit

---
+++ MyTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyTimer
+//
+//  Created by Takayama on 2025/10/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by Takayama on 2025/10/28.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
