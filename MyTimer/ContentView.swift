2a93a42a272877f65ae79aa7cf54cfbf603c5976
Takayama <tkym1982@gmail.com>
2025-10-30 21:20:41 +0900

    コメント追加

--- MyTimer/ContentView.swift
+++ MyTimer/ContentView.swift
@@ -8,16 +8,102 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var timerHandler : Timer?
+    
+    @State var count = 0
+    
+    @AppStorage("timer_value") var timerValue = 10
+    
+    @State var showAlert = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                Image("backgroundTimer")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .scaledToFill()
+                VStack(spacing:30.0) {
+                    Text("残り\(timerValue - count)秒")
+                        .font(.largeTitle)
+                    HStack {
+                        Button {
+                            //スタート時の動作
+                            startTimer()
+                        } label: {
+                            Text("スタート")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width: 140, height: 140)
+                                .background(Color("startColor"))
+                                .clipShape(Circle())
+                        }
+                        
+                        Button {
+                            //ストップ時の動作
+                            if let unwrappedTimer = timerHandler {
+                                if unwrappedTimer.isValid == true {
+                                    unwrappedTimer.invalidate( )
+                                }
+                            }
+                        } label: {
+                            Text("ストップ")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width: 140, height: 140)
+                                .background(Color("stopColor"))
+                                .clipShape(Circle())
+                        }
+                    }
+                }
+            }
+            .onAppear {
+                count = 0
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink {
+                        SettingView()
+                    } label: {
+                        Text("秒数設定")
+                    }
+                }
+            }
+            .alert("終了", isPresented: $showAlert) {
+                Button("OK") {
+                    print("OKタップされました")
+                }
+            } message: {
+                Text("タイマー終了時間です")
+            }
         }
-        .padding()
+    }
+    
+    func countDownTimer() {
+        count += 1
+        if timerValue - count <= 0 {
+            timerHandler?.invalidate()
+            showAlert = true
+        }
+    }
+    
+    func startTimer() {
+        if let unwrappedTimerHandler = timerHandler {
+            if unwrappedTimerHandler.isValid == true {
+                return
+            }
+        }
+        
+        if timerValue - count <= 0 {
+            count = 0
+        }
+        
+        timerHandler = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            countDownTimer()
+        }
     }
 }
+
 
 #Preview {
     ContentView()
---
+++ MyTimer/SettingView.swift
@@ -0,0 +1,51 @@
+//
+//  SettingView.swift
+//  MyTimer
+//
+//  Created by Takayama on 2025/10/28.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    @AppStorage("timer_value") var timerValue = 10
+    
+    var body: some View {
+        ZStack {
+            Color("backgroundSetting")
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text("\(timerValue)秒")
+                    .font(.largeTitle)
+                
+                Spacer()
+                //10秒毎
+                Picker(selection: $timerValue) {
+                    Text("10")
+                        .tag(10)
+                    Text("20")
+                        .tag(20)
+                    Text("30")
+                        .tag(30)
+                    Text("40")
+                        .tag(40)
+                    Text("50")
+                        .tag(50)
+                    Text("60")
+                        .tag(60)
+                }label: {
+                    Text("選択")
+                }
+                .pickerStyle(.wheel)
+                Spacer()
+            }
+        }
+    }
+}
+
+#Preview {
+    SettingView()
+}
